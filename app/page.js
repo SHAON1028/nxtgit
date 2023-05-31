c42b79b77e3dce96d83280e5a047dc7d827b2ddc
“SHAON1028” <shaonkhairul@gmail.com>
2023-05-31 10:14:48 +0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to my Github</h1>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/about/team">Team</Link></li>
+      </ul>
+    </div>
+  )
+}
+
+export default HomePage
